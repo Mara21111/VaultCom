12606cfc4529d4b219c2fb4b5d18e8a18bf4a521
Marek Děkan <dekanmarek@sssvt.cz>
2025-04-01 22:39:11 +0200

    links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,24 @@
 import { Routes } from '@angular/router';
 import {LoginPageComponent} from './Pages/login-page/login-page.component';
 import {SignInPageComponent} from './Pages/sign-in-page/sign-in-page.component';
+import { AdminAllUsersPageComponent } from './Pages/admin-all-users-page/admin-all-users-page.component';
+import { AdminMenuPageComponent } from './Pages/admin-menu-page/admin-menu-page.component';
+import { AdminUserInfoPageComponent } from './Pages/admin-user-info-page/admin-user-info-page.component';
+import { PublicChatsPageComponent } from './Pages/public-chats-page/public-chats-page.component';
+import { ReportsPageComponent } from './Pages/reports-page/reports-page.component';
+import { UserFriendsPageComponent } from './Pages/user-friends-page/user-friends-page.component';
+import { UserProfilePageComponent } from './Pages/user-profile-page/user-profile-page.component';
+import { MainPageComponent } from './Pages/main-page/main-page.component';
 
 export const routes: Routes = [
   { path: '', component: LoginPageComponent},
-  { path: 'sigin', component: SignInPageComponent}
+  { path: 'sigin', component: SignInPageComponent},
+  { path: 'admin-all-users', component: AdminAllUsersPageComponent},
+  { path: 'admin-menu', component: AdminMenuPageComponent},
+  { path: 'admin-user-info', component: AdminUserInfoPageComponent},
+  { path: 'public-chats', component: PublicChatsPageComponent},
+  { path: 'reports', component: ReportsPageComponent},
+  { path: 'user-friends', component: UserFriendsPageComponent},
+  { path: 'user-profile', component: UserProfilePageComponent},
+  { path: 'main', component: MainPageComponent}
 ];
