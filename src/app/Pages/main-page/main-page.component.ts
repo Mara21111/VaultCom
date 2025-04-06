9d092092ea4dd394fd5230ba98047396b22cf121
Marek Děkan <dekanmarek@sssvt.cz>
2025-04-06 20:19:50 +0200

    main

--- src/app/Pages/main-page/main-page.component.ts
+++ src/app/Pages/main-page/main-page.component.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
