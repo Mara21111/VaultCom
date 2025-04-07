ce5ff907a0982aa2c71e31533b5d3a7bb9105f1d
Marek Děkan <dekanmarek@sssvt.cz>
2025-04-07 22:54:53 +0200

    reports

--- src/app/Pages/reports-page/reports-page.component.ts
+++ src/app/Pages/reports-page/reports-page.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Report_log } from '../../models/Report_log';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-reports-page',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgFor],
   templateUrl: './reports-page.component.html',
   styleUrl: './reports-page.component.scss'
 })
 export class ReportsPageComponent {
 
+  reports: Report_log[] = [
+    { user_id: 1, reported_user_id: 2, message: "test"},
+    { user_id: 1, reported_user_id: 2, message: "test"},
+    { user_id: 1, reported_user_id: 2, message: "test"},
+    { user_id: 1, reported_user_id: 2, message: "test"},
+    { user_id: 1, reported_user_id: 2, message: "test"},
+    { user_id: 1, reported_user_id: 2, message: "test"},
+    { user_id: 1, reported_user_id: 2, message: "test"},
+    { user_id: 1, reported_user_id: 2, message: "test"},
+    { user_id: 1, reported_user_id: 2, message: "test"},
+    { user_id: 1, reported_user_id: 2, message: "test"},
+    { user_id: 1, reported_user_id: 2, message: "test"},
+    { user_id: 1, reported_user_id: 2, message: "test"},
+  ];
 }
