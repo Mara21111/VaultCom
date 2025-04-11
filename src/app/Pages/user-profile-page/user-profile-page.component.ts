35a714bc1535eb01735633b0c462a8d433dfeab2
Jok55r <boyblesk@gmail.com>
2025-04-11 22:19:45 +0200

    user-profile rebuilt from the ground (cleaned code)

--- src/app/Pages/user-profile-page/user-profile-page.component.ts
+++ src/app/Pages/user-profile-page/user-profile-page.component.ts
@@ -6,6 +6,22 @@ import { Component } from '@angular/core';
   templateUrl: './user-profile-page.component.html',
   styleUrl: './user-profile-page.component.scss'
 })
+
+// TOTALNE ZADRENY TO JE ↓↓↓ (zkrachuje celej angular v konzoli jestli se tohle odkomentuje)
+/*
+@Component({
+  selector: 'app-on-off-toggle',
+  templateUrl: './on-off-toggle.component.html',
+  styleUrls: ['./on-off-toggle.component.scss']
+})
+export class OnOffToggleComponent {
+  isOn = false; // Initial state
+
+  toggleState() {
+    this.isOn = !this.isOn; // Toggle the state
+  }
+}*/
+
 export class UserProfilePageComponent {
 
 }
