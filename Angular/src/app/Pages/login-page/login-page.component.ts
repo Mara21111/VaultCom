c79faac8e36223922f188cb1568a353292e9addd
ts-pmo-icl <vasek.svitil39@gmail.com>
2025-04-21 22:19:15 +0200

    vytvoreni public chatu

--- Angular/src/app/Pages/login-page/login-page.component.ts
+++ Angular/src/app/Pages/login-page/login-page.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-
 import { FormsModule } from '@angular/forms';
 
 @Component({
--- Angular/src/app/Pages/public-chats-page/public-chats-page.component.ts
+++ Angular/src/app/Pages/public-chats-page/public-chats-page.component.ts
@@ -3,11 +3,12 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 import { DataService } from '../../services/data.service';
 import { NgFor } from '@angular/common';
 import { BaseUiComponent } from "../base-ui/base-ui.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-public-chats-page',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, NgFor, BaseUiComponent],
+  imports: [RouterLink, RouterLinkActive, NgFor, BaseUiComponent, FormsModule],
   templateUrl: './public-chats-page.component.html',
   styleUrl: './public-chats-page.component.scss'
 })
@@ -16,4 +17,17 @@ export class PublicChatsPageComponent {
 
   data = inject(DataService)
 
+  chatName: string = "";
+
+  onClick() {
+    this.data.chats.push({
+      id: this.data.chats.length + 1,
+      is_public: true,
+      name: this.chatName,
+      description: '',
+      creator_id: 1
+    })
+
+    this.chatName = '';
+  }
 }
