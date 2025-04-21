36e24d966bf34c453b6a75a7dd5416429bafbaea
ts-pmo-icl <vasek.svitil39@gmail.com>
2025-04-21 22:04:35 +0200

    login se zapisuje

--- Angular/src/app/Pages/login-page/login-page.component.ts
+++ Angular/src/app/Pages/login-page/login-page.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-login-page',
-  imports: [RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, FormsModule ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
+  name: string = '';
+  password: string = '';
 
+  onLogin() {
+    console.log('Email:', this.name);
+    console.log('Password:', this.password);
+  }
 }
