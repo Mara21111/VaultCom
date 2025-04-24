91181a6abc3613ed1ab2182c0e1ec29b74e9e5da
ts-pmo-icl <vasek.svitil39@gmail.com>
2025-04-24 20:28:58 +0200

    message se vypise

--- Angular/src/app/Pages/main-page/main-page.component.ts
+++ Angular/src/app/Pages/main-page/main-page.component.ts
@@ -3,11 +3,12 @@ import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { BaseUiComponent } from "../base-ui/base-ui.component";
 import { DataService } from '../../services/data.service';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive, BaseUiComponent, NgFor],
+  imports: [CommonModule, RouterLink, RouterLinkActive, BaseUiComponent, NgFor, FormsModule ] ,
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
@@ -15,6 +16,8 @@ export class MainPageComponent {
   
   data = inject(DataService)
   public_chats: boolean = false;
+  
+  message: string = '';
 
   changeChatsToPublic(){
     this.public_chats = true;
@@ -24,19 +27,8 @@ export class MainPageComponent {
     this.public_chats = false;
   }
 
-  formatDate(timestamp: string): string {
-    const messageDate = new Date(timestamp);  // Convert the string timestamp to a Date object
-    const now = new Date();
-    
-    const diff = now.getTime() - messageDate.getTime();
-    const hoursDifference = diff / (1000 * 3600);
-    
-    if (hoursDifference < 24) {
-      // If the message was sent within the last 24 hours, show the time
-      return messageDate.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});  // Time format (HH:mm)
-    } else {
-      // If the message was sent more than 24 hours ago, show the date (DD.MM)
-      return `${messageDate.getDate()}.${messageDate.getMonth() + 1}`;
-    }
+  sendMessage(form: NgForm){
+    console.log(this.message)
+    this.message = '';
   }
 }
