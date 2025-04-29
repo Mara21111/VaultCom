e8905dfd0413811d714728d17dec09ee5f95ff77
Václav Svítil <SvitilVaclav@litv.sssvt.cz>
2025-04-29 08:34:56 +0200

    small changes

--- WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1be1b5257898d7161fd7566e030da25bf584de51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6f8a20b826d9b95fec8c0ea0575886901073c69")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
