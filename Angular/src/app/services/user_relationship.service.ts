9150a5cd09b1fd2ed87ca20e3514c068b6b961a6
ts-pmo-icl <vasek.svitil39@gmail.com>
2025-05-03 16:16:42 +0200

    user relationship service

---
+++ Angular/src/app/services/user_relationship.service.ts
@@ -0,0 +1,57 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User_relationship } from '../models/User_relationship';
+import { User } from '../models/User'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserRelationshipService {
+
+  public constructor(private http: HttpClient, private Service: User_relationship) {}
+
+  public getAllFriends(user_id: number): Observable<User[]> {
+    return this.http.get<User[]>('http://localhost:5000/api/UserRelationship/friends-of-user' + user_id)
+  }
+
+  public getAllIncomingRequests(user_id: number): Observable<User[]> {
+    return this.http.get<User[]>('http://localhost:5000/api/UserRelationship/pending-requests-to-user' + user_id)
+  }
+
+  public getAllOutgoingRequests(user_id: number): Observable<User[]> {
+    return this.http.get<User[]>('http://localhost:5000/api/UserRelationship/pending-requests-from-user' + user_id)
+  }
+
+  public sendRequest(sender_id: number, reciever_id: number): Observable<void> {
+    return this.http.get<void>('http://localhost:5000/api/UserRelationship/send-request-from' + sender_id + '-to' + reciever_id)
+  }
+
+  public cancelRequest(sender_id: number, reciever_id: number): Observable<void> {
+    return this.http.get<void>('http://localhost:5000/api/UserRelationship/cancel-request-from' + sender_id + '-to' + reciever_id)
+  }
+
+  public acceptRequest(sender_id: number, reciever_id: number): Observable<void> {
+    return this.http.get<void>('http://localhost:5000/api/UserRelationship/accept-request-from' + sender_id + '-to' + reciever_id)
+  }
+
+  public rejectRequest(sender_id: number, reciever_id: number): Observable<void> {
+    return this.http.get<void>('http://localhost:5000/api/UserRelationship/reject-request-from' + sender_id + '-to' + reciever_id)
+  }
+
+  public toggleBlockUser(user_id: number, blocked_user_id: number): Observable<void> {
+    return this.http.get<void>('http://localhost:5000/api/UserRelationship/toggle-' + user_id + '-block-' + blocked_user_id)  //toggle-{user_id}-block-{blocked_user_id}
+  }
+
+  public toggleMuteUser(user_id: number, muted_user_id: number): Observable<void> {
+    return this.http.get<void>('http://localhost:5000/api/UserRelationship/toggle-' + user_id + '-mute-' + muted_user_id)
+  }
+
+  public changeNickname(user_id: number, friend_id: number): Observable<void> {
+    return this.http.get<void>('http://localhost:5000/api/UserRelationship/change-' + user_id + '-nickname-of-' + friend_id)
+  }
+
+  public unfriendUser(user_id: number, friend_id: number): Observable<void> {
+    return this.http.get<void>('http://localhost:5000/api/UserRelationship/unfriend-' + friend_id + '-from-' + user_id)
+  }
+}
