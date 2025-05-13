50b22ddfa1fb67f5eb1e47202dc2f01a963ea6c9
Jok55r <boyblesk@gmail.com>
2025-05-13 09:46:34 +0200

    rewritten login with new logic into implementations

---
+++ WebApplication1/WebApplication1/Models/DTO/LoginDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.Models.DTO
+{
+    public class LoginDTO
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+    public class AuthResult
+    {
+        public bool Success { get; set; }
+        public string Token { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ WebApplication1/WebApplication1/Services/Implementations/AuthenticationService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Org.BouncyCastle.Asn1.Ocsp;
+using Org.BouncyCastle.Asn1.X509;
+using WebApplication1.Models.Data;
+using WebApplication1.Models.DTO;
+using WebApplication1.Services.Interfaces;
+
+namespace WebApplication1.Services.Implementations
+{
+    public class AuthenticationService : Interfaces.IAuthenticationService
+    {
+        private readonly MyContext context;
+        private readonly MyContext _context;
+        private readonly TokenService _tokenService;
+
+        public AuthenticationService(MyContext context, TokenService tokenService)
+        {
+            _context = context;
+            _tokenService = tokenService;
+        }
+
+        public async Task<AuthResult> LoginAsync(LoginDTO loginDto)
+        {
+            var user = _context.User.FirstOrDefault(x => x.Username == loginDto.Username);
+
+            if (user != null && loginDto.Password == user.Password)
+            {
+                var token = _tokenService.Create(user);
+
+                return new AuthResult
+                {
+                    Success = true,
+                    Token = token
+                };
+            }
+
+            return new AuthResult
+            {
+                Success = false,
+                Message = "Invalid username or password"
+            };
+        }
+
+    }
+}
