3cd9d313a35b20f1cc2c6ef89ec9aca2ddb00e7f
Jok55r <boyblesk@gmail.com>
2025-05-21 10:04:31 +0200

    Revert "new gitignore???"
    This reverts commit ddb88b17ae441b343ad06ca6d5049dd66fd27172.
    
    # Conflicts:
    #	WebApplication1/.gitignore
    #	WebApplication1/.vs/WebApplication1/DesignTimeBuild/.dtbcache.v2
    #	WebApplication1/.vs/WebApplication1/v17/.suo
    #	WebApplication1/.vs/WebApplication1/v17/DocumentLayout.backup.json
    #	WebApplication1/.vs/WebApplication1/v17/DocumentLayout.json
    #	WebApplication1/WebApplication1/bin/Debug/net8.0/WebApplication1.dll
    #	WebApplication1/WebApplication1/bin/Debug/net8.0/WebApplication1.exe
    #	WebApplication1/WebApplication1/bin/Debug/net8.0/WebApplication1.pdb
    #	WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
    #	WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfoInputs.cache
    #	WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.dll
    #	WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.pdb
    #	WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.sourcelink.json
    #	WebApplication1/WebApplication1/obj/Debug/net8.0/apphost.exe
    #	WebApplication1/WebApplication1/obj/Debug/net8.0/ref/WebApplication1.dll
    #	WebApplication1/WebApplication1/obj/Debug/net8.0/refint/WebApplication1.dll

--- WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc3472b08e67f4d40dd632a562089ea3630dffc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d908a0a8358d4eeece276649c5a022ce8ec4627")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
